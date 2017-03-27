4087a50b613e5d0fc4443a488ee01302699d1f4a
Felix Ruiz de Arcaute <felix@Felixs-MacBook-Air.local>
2017-03-27 07:48:59 +0200

    removed authorization files as it doen't seem needed

--- config/authorization/resources/repository.lisp
+++
@@ -1,7 +0,0 @@
-(in-package :mu-cl-resources)
-
-(add-prefix "auth" "http://mu.semte.ch/vocabularies/authorization/")
-(add-prefix "foaf" "http://xmlns.com/foaf/0.1/")
-(add-prefix "mu" "http://mu.semte.ch/vocabularies/core/")
-(add-prefix "dct" "http://purl.org/dc/terms/")
-(add-prefix "ext" "http://mu.semte.ch/vocabularies/ext/")
